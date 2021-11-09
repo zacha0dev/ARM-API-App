93f3474b4c6c51917de961dd19448e5c5111ecda
Zachary Allen <zacha0dev@gmail.com>
2021-11-09 14:04:19 -0500

    Add Liability Statement

--- Python/azureRESTapi.py
+++ Python/azureRESTapi.py
@@ -1,3 +1,13 @@
+#------------------------------------------------------------------------------   
+#   
+#    
+# THIS CODE AND ANY ASSOCIATED INFORMATION ARE PROVIDED “AS IS” WITHOUT   
+# WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT   
+# LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS   
+# FOR A PARTICULAR PURPOSE. THE ENTIRE RISK OF USE, INABILITY TO USE, OR    
+# RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.   
+#   
+#------------------------------------------------------------------------------ 
 
 # Title: ARM REST API App 
 # Author: Zachary Allen 
